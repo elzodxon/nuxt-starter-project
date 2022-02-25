163a29d35cc4fa7d094d0920724007acf18064aa
Elzodxon Sharofaddinov <elzodxon@gmail.com>
2022-02-25 19:45:20 +0500

    remove: tailwind default config

--- tailwind.config.js
+++ tailwind.config.js
@@ -9,23 +9,5 @@ module.exports = {
       `nuxt.config.{js,ts}`,
     ],
   },
-  theme: {
-    screens: {
-      a: { max: '1200px' },
-      b: { max: '1024px' },
-      c: { max: '900px' },
-      d: { max: '768px' },
-      e: { max: '600px' },
-      f: { max: '480px' },
-      g: { max: '375px' },
-    },
-    extend: {
-      colors: {
-        pink: '#F00073',
-        red: '#FF0000',
-        blue: '#0088CC',
-        dopeBlue: '#1877F2',
-      },
-    },
-  },
+  theme: {},
 }
